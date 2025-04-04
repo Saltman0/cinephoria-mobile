54339f1ac44cc36a0b44948c82339286a83e56b5
Saltman0 <baudoin.mathieu@protonmail.com>
2025-04-04 18:56:13 +0200

    Booking list component updated

--- src/components/booking-list/booking-list.component.ts
+++ src/components/booking-list/booking-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
+import {BookingComponent} from "../booking/booking.component";
+import {DatabaseService} from "../../services/database/database.service";
+import {BookingRenderer} from "../../renderers/booking.renderer";
+import {BookingModel} from "../../models/booking.model";
 
 @Component({
   selector: 'app-booking-list',
   standalone: true,
   templateUrl: './booking-list.component.html',
   styleUrls: ['./booking-list.component.scss'],
+  imports: [HeaderComponent, BookingComponent]
 })
 export class BookingListComponent implements OnInit {
 
-  constructor() { }
+  bookingList: any[] = [];
 
-  ngOnInit() {}
+  constructor(private readonly databaseService: DatabaseService, private readonly bookingRenderer: BookingRenderer) {}
+
+  async ngOnInit(): Promise<void> {
+    const bookings: BookingModel[] = await this.databaseService.getBookings();
+    bookings.forEach(booking => {
+      this.bookingList.push(this.bookingRenderer.render(booking));
+    });
+  }
 
 }
