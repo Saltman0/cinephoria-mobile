98a651130df5d0be5f1fbc1dbff50729b051aa60
Saltman <mathieu.baudoin@evalandgo.com>
2025-04-12 20:22:52 +0200

    Booking detail component updated

--- src/components/booking-detail/booking-detail.component.ts
+++ src/components/booking-detail/booking-detail.component.ts
@@ -3,6 +3,7 @@ import {HeaderComponent} from "../header/header.component";
 import {QRCodeComponent} from "angularx-qrcode";
 import {NgOptimizedImage} from "@angular/common";
 import {BookingRenderer} from "../../renderers/booking.renderer";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-booking-list',
@@ -13,17 +14,20 @@ import {BookingRenderer} from "../../renderers/booking.renderer";
 })
 export class BookingDetailComponent implements OnInit {
 
-  @Input() movieTitle: string = "Nom du film actuel";
-  @Input() movieImage: string = "Lien vers l'image";
-  @Input() showtimeDate: string = "Date du film actuel";
-  @Input() showtimeStartHour: string = "xxhxx";
-  @Input() showtimeEndHour: string = "xxhxx";
-  @Input() hallNumber: number = 777;
-  @Input() seats: string = "A0/B0/C0";
+  @Input() movieTitle: string = "Avengers : Infinity War";
+  @Input() movieImage: string = "Avengers - Infinity War.png";
+  @Input() showtimeDate: string = "12/04/2025";
+  @Input() showtimeStartHour: string = "21h00";
+  @Input() showtimeEndHour: string = "23h30";
+  @Input() hallNumber: number = 1;
+  @Input() seats: string = "A22/A23/A24";
   @Input() qrCode: string = "https://github.com/Cordobo/angularx-qrcode";
 
-  constructor(private readonly bookingRenderer: BookingRenderer) {}
+  constructor(private readonly router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 
+  returnToBookingList(): void {
+    this.router.navigate(['/booking-list']);
+  }
 }
