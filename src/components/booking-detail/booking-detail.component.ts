b81c44676b75e253421e98ad7df5a8009e61e850
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-10 22:47:16 +0200

    :sparkles: Booking detail component updated

--- src/components/booking-detail/booking-detail.component.ts
+++ src/components/booking-detail/booking-detail.component.ts
@@ -2,7 +2,9 @@ import {Component, Input} from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {QRCodeComponent} from "angularx-qrcode";
 import {NgOptimizedImage, NgStyle} from "@angular/common";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {BookingRenderer} from "../../renderers/booking.renderer";
+import {Booking, DatabaseService} from "../../services/database/database.service";
 
 @Component({
   selector: 'app-booking-list',
@@ -14,16 +16,40 @@ import {Router} from "@angular/router";
 export class BookingDetailComponent  {
 
   @Input() id: number = 0;
-  @Input() movieTitle: string = "Titre du film";
-  @Input() movieImage: string = "Titre_du_film.png";
-  @Input() showtimeDate: string = "12/04/2025";
-  @Input() showtimeStartHour: string = "21h00";
-  @Input() showtimeEndHour: string = "23h30";
-  @Input() hallNumber: number = 1;
-  @Input() seats: string = "A22/A23/A24";
-  @Input() qrCode: string = "https://github.com/Cordobo/angularx-qrcode";
+  @Input() movieTitle: string|null = "Titre du film";
+  @Input() movieImage: string|null = "Titre_du_film.png";
+  @Input() showtimeDate: string|null = "Date du film actuel";
+  @Input() showtimeStartHour: string|null = "xx";
+  @Input() showtimeStartMinute: string|null = "xx";
+  @Input() showtimeEndHour: string|null = "xx";
+  @Input() showtimeEndMinute: string|null = "xx";
+  @Input() hallNumber: number|null = 1;
+  @Input() seats: string|null = "A22/A23/A24";
+  @Input() qrCode: string|null = "https://github.com/Cordobo/angularx-qrcode";
 
-  constructor(private readonly router: Router) {}
+  constructor(private readonly databaseService: DatabaseService,
+              private readonly bookingRenderer: BookingRenderer,
+              private readonly router: Router,
+              private readonly activatedRoute: ActivatedRoute) {}
+
+  async ngOnInit(): Promise<void> {
+    const bookingId = Number(this.activatedRoute.snapshot.paramMap.get('bookingId'));
+
+    const booking: Booking|null = await this.databaseService.getBooking(bookingId) ?? null;
+    if (booking !== null) {
+      const result = await this.bookingRenderer.render(booking);
+      this.movieTitle = result.movieTitle;
+      this.movieImage = result.movieImage;
+      this.showtimeDate = result.showtimeDate;
+      this.showtimeStartHour = result.showtimeStartHour;
+      this.showtimeStartMinute = result.showtimeStartMinute;
+      this.showtimeEndHour = result.showtimeEndHour;
+      this.showtimeEndMinute = result.showtimeEndMinute;
+      this.hallNumber = result.hallNumber;
+      this.seats = result.seats;
+      this.qrCode = result.qrCode;
+    }
+  }
 
   returnToBookingList(): void {
     this.router.navigate(['/booking-list']);
