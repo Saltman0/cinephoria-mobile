ad4636f79df648a7c92289216ad9cfd555a7c312
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-22 01:38:29 +0200

    ✅ Local storage service : unit test updated

--- src/services/local-storage/local-storage.service.spec.ts
+++ src/services/local-storage/local-storage.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { LocalStorageService } from './local-storage.service';
+import {LocalStorageService} from './local-storage.service';
 
 describe('LocalStorageService', () => {
-    let service: LocalStorageService;
+  let service: LocalStorageService;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(LocalStorageService);
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LocalStorageService);
+    localStorage.clear();
+
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getJwtToken', () => {
+
+    it('should return a JWT token from localStorage', () => {
+      const token = 'test-jwt-token';
+      localStorage.setItem('jwtToken', token);
+      expect(service.getJwtToken()).toBe(token);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should return null if token is not in localStorage', () => {
+      const result: string = service.getJwtToken();
+
+      expect(result).toBeNull();
     });
+
+  });
+
+  describe('addJwtToken', () => {
+
+    it('should add a JWT token to localStorage', () => {
+      const token = 'test-jwt-token';
+      service.addJwtToken(token);
+      expect(localStorage.getItem('jwtToken')).toBe(token);
+    });
+
+  });
+
+  describe('deleteJwtToken', () => {
+
+    it('should delete a JWT token from localStorage', () => {
+      const token = 'test-jwt-token';
+      localStorage.setItem('jwtToken', token);
+
+      service.deleteJwtToken();
+      expect(localStorage.getItem('jwtToken')).toBeNull();
+    });
+
+  });
+
 });
