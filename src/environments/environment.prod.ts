01a94e1547202b8c6770ac40b6e48bfea1476439
Saltman0 <baudoin.mathieu@protonmail.com>
2026-02-03 22:26:26 +0100

    ✨ Prod env variables updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  BOOKING_API_URL: "http://172.18.0.5/",
+  GRAPHQL_API_URL: "http://172.18.0.5/",
+  INFRASTRUCTURE_API_URL: "http://172.18.0.5/",
+  MOVIE_API_URL: "http://172.18.0.5/",
+  SHOWTIME_API_URL: "http://172.18.0.5/",
+  USER_API_URL: "http://172.18.0.5/"
 };
